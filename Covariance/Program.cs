01460673be5222fd979b75d3da76e09d0597b7c5
Andrei Mungiu <andrei_mungiu@yahoo.com>
2017-09-14 09:51:51 +0200

    Tutorial checkpoint

--- Covariance/Program.cs
+++ Covariance/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Covariance
 {
@@ -6,7 +7,24 @@ namespace Covariance
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //casting a derived type to the base type
+            string str = "Hello World!";
+            object obj = str;
+
+            var strList = new string[] { "Monday", "Tuesday" };
+            //works since Enumerator & Enumerable are read-only, can not modify col.
+            //this is why Microsoft allows covariance for them
+            IEnumerable<object> objEnum = strList;
+
+            ////List<string> != List<object>
+            //List<object> objList = (List<object>)strList;
+
+            ////Array covariance: can cast array of derived type to base type
+            ////works for retrieving items from array but breaks when tryingt o 
+            ////set items, because not all objects are strings
+            //object[] objArray = strArray;
+            ////does not work because "int" != "string"
+            //objArray[0] = 5;
         }
     }
 }
